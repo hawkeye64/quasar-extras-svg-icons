ed3de26f5558201701950566828ed8826382427f
Jeff Galbraith <galbraith64@gmail.com>
2021-12-03 13:30:19 -0700

    chore(docs): change "Entypo Icons" to "Entypo+ Icons"

--- docs/src/components/page-parts/IconFinder.vue
+++ docs/src/components/page-parts/IconFinder.vue
@@ -84,7 +84,7 @@ export default defineComponent({
         { label: 'Codicons', value: 'codicons' },
         { label: 'Cool Icons', value: 'cool-icons' },
         { label: 'Country Flag Icons', value: 'country-flag-icons' },
-        { label: 'Entypo Icons', value: 'entypo-icons' },
+        { label: 'Entypo+ Icons', value: 'entypo-icons' },
         { label: 'FluentUI System Icons', value: 'fluentui-system-icons' },
         { label: 'Hero Icons (solid)', value: 'hero-icons-solid' },
         { label: 'Hero Icons (outline)', value: 'hero-icons-outline' },
