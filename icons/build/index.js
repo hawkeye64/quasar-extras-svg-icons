73cf3d1bdcd5ae6afbfe2c37fc05656e8f9ebe44
Jeff Galbraith <galbraith64@gmail.com>
2021-12-04 16:44:24 -0700

    feat: use the Queue system for processing icon-sets
    The previous version would run each of the icon-set processing in a new thread. But, there are many more icon-sets than cores on a computer. If we use a Queue system with `(cpus().length - 1 || 1)` for concurrency, then it will not peg out the computer and should be faster.
    Also, we can now get total icons, total processing time, as well as generate an icon-info.json file.

--- icons/build/index.js
+++ icons/build/index.js
@@ -1,41 +1,91 @@
-const parallel = require('os').cpus().length > 1
+const { writeFileSync } = require('fs')
+const cpus = require('os').cpus().length
+const parallel = cpus > 1
+const maxJobCount = cpus - 1 || 1
 const run = parallel ? require('child_process').fork : require
-const { join } = require('path')
+const { resolve, join } = require('path')
+const { Queue, sleep, retry } = require('./utils')
 
-function runJob (scriptFile) {
-  run(join(__dirname, scriptFile))
+async function generate () {
+  let totalTime = 0
+  let totalIcons = 0
+  const iconSets = {}
+
+  function handleChild (child) {
+    return new Promise((resolve, reject) => {
+      // watch for exit event
+      child.on('exit', (code, signal) => {
+        resolve()
+      })
+
+      // watch for message event
+      child.on('message', message => {
+        iconSets[ message.distName ] = message.iconNames
+        totalIcons += message.iconNames.length
+        totalTime += message.time
+      })
+    })
+  }
+
+  const queue = new Queue(
+    async (scriptFile) => {
+      await retry(async ({ tries }) => {
+        await sleep((tries - 1) * 100)
+        const child = run(join(__dirname, scriptFile))
+        await handleChild(child)
+      })
+    },
+    { concurrency: maxJobCount },
+  )
+
+  function runJob (scriptFile) {
+    if (parallel) {
+      queue.push(scriptFile)
+      return
+    }
+    return run(join(__dirname, scriptFile))
+  }
+
+  runJob('./akar-icons.js')
+  runJob('./ant-design-icons.js')
+  runJob('./box-icons.js')
+  runJob('./carbon-icons.js')
+  runJob('./carbon-pictograms.js')
+  runJob('./codicons.js')
+  runJob('./cool-icons.js')
+  runJob('./country-flag-icons.js')
+  runJob('./entypo-icons.js')
+  runJob('./fluentui-system-icons.js')
+  runJob('./health-icons.js')
+  runJob('./hero-icons-outline.js')
+  runJob('./hero-icons-solid.js')
+  runJob('./grid-icons.js')
+  runJob('./icomoon-free-icons.js')
+  runJob('./iconoir-icons.js')
+  runJob('./jam-icons.js')
+  runJob('./material-line-icons.js')
+  runJob('./webfont-medical-icons.js')
+  runJob('./oct-icons.js')
+  runJob('./pixelart-icons.js')
+  runJob('./prime-icons.js')
+  runJob('./radix-ui-icons.js')
+  runJob('./remix-icons.js')
+  runJob('./simple-icons.js')
+  runJob('./system-uicons.js')
+  runJob('./teeny-icons.js')
+  runJob('./tabler-icons.js')
+  runJob('./uiw-icons.js')
+  runJob('./unicons.js')
+  runJob('./vaadin-icons.js')
+  runJob('./weather-icons.js')
+  runJob('./zond-icons.js')
+
+  await queue.wait({ empty: true })
+
+  console.log(`Total Time: ${ totalTime }ms`)
+  console.log('Icon Count:', totalIcons)
+  const file = resolve(__dirname, '../icon-info.json')
+  writeFileSync(file, JSON.stringify(iconSets, null, 2), 'utf-8')
 }
 
-runJob('./akar-icons.js')
-runJob('./ant-design-icons.js')
-runJob('./box-icons.js')
-runJob('./carbon-icons.js')
-runJob('./carbon-pictograms.js')
-runJob('./codicons.js')
-runJob('./cool-icons.js')
-runJob('./country-flag-icons.js')
-runJob('./entypo-icons.js')
-runJob('./fluentui-system-icons.js')
-runJob('./health-icons.js')
-runJob('./hero-icons-outline.js')
-runJob('./hero-icons-solid.js')
-runJob('./grid-icons.js')
-runJob('./icomoon-free-icons.js')
-runJob('./iconoir-icons.js')
-runJob('./jam-icons.js')
-runJob('./material-line-icons.js')
-runJob('./webfont-medical-icons.js')
-runJob('./oct-icons.js')
-runJob('./pixelart-icons.js')
-runJob('./prime-icons.js')
-runJob('./radix-ui-icons.js')
-runJob('./remix-icons.js')
-runJob('./simple-icons.js')
-runJob('./system-uicons.js')
-runJob('./teeny-icons.js')
-runJob('./tabler-icons.js')
-runJob('./uiw-icons.js')
-runJob('./unicons.js')
-runJob('./vaadin-icons.js')
-runJob('./weather-icons.js')
-runJob('./zond-icons.js')
+generate()
