38e34a5642083b0cec1058fd485d50c459071466
Jeff <galbraith64@gmail.com>
2023-01-19 07:59:49 -0700

    feat(icons): update Carbon pictograms => v12.11.0

--- icons/carbon-pictograms-v12/index.d.ts
+++ icons/carbon-pictograms-v12/index.d.ts
@@ -1,4 +1,4 @@
-/* Carbon Pictograms v12.10.0 */
+/* Carbon Pictograms v12.11.0 */
 
 export declare const carpicAcceleratedComputing: string;
 export declare const carpicAcceleratingTransformation: string;
