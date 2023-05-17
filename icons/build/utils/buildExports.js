788cbca3f1657fb24fe013c6f63c82317d229d40
Jeff <galbraith64@gmail.com>
2023-05-17 10:53:01 -0600

    feat(build): improved mjs and cjs (js) handling in package.json

--- icons/build/utils/buildExports.js
+++ icons/build/utils/buildExports.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const fse = require('fs-extra');
-const path = require('path');
+const fs = require('fs')
+const fse = require('fs-extra')
+const path = require('path')
 
-const baseFolder = path.join(__dirname, '../..');
+const baseFolder = path.join(__dirname, '../..')
 
 // files and folders to skip
 const skips = [
@@ -14,70 +14,80 @@ const skips = [
   'package.json',
   'README.md',
   'yarn.lock'
-];
+]
+
+const extensionList = [
+  { prop: 'types', ext: '.d.ts' },
+  { prop: 'import', ext: '.mjs' },
+  { prop: 'require', ext: '.js' }
+]
 
 async function readFolders(baseFolder, skips = []) {
   try {
-    const files = await fs.promises.readdir(baseFolder);
-    const folders = [];
+    const files = await fs.promises.readdir(baseFolder)
+    const folders = []
 
     for (const file of files) {
       if (!skips.includes(file) || file.startsWith('.')) {
-        const filePath = path.join(baseFolder, file);
-        const stats = await fs.promises.stat(filePath);
+        const filePath = path.join(baseFolder, file)
+        const stats = await fs.promises.stat(filePath)
 
         if (stats.isDirectory()) {
-          folders.push(file);
+          folders.push(file)
         }
       }
     }
 
-    return folders;
+    return folders
   }
   catch (err) {
-    throw new Error('Error reading directory: ' + err);
+    throw new Error('Error reading directory: ' + err)
   }
 }
 
 function generateExports(folders) {
   const exports = {
     '.': './index.js'
-  };
-
-  for (const folder of folders) {
-    exports[ `./${ folder }` ] = {
-      "`./${ folder }/icons.json`": `./${ folder }/icons.json`,
-      types: `./${ folder }/index.d.ts`,
-      import: `./${ folder }/index.mjs`,
-      require: `./${ folder }/index.js`
-    };
   }
 
-  // exports[ './package.json' ] = './package.json'
+  for (const folder of folders) {
+    const exportDefinition = extensionList.reduce((acc, { prop, ext }) => {
+      const filePath = path.join(baseFolder, folder, `index${ ext }`)
+      if (fs.existsSync(filePath)) {
+        acc[ prop ] = `./${ folder }/index${ ext }`
+      }
+      return acc
+    }, {})
+
+    if (Object.keys(exportDefinition).length !== 0) {
+      exports[ `./${ folder }` ] = exportDefinition
+    }
+  }
+
   exports[ './*' ] = './*'
 
-  return exports;
+  return exports
 }
 
 async function updatePackageJson(exports) {
   try {
-    const packageJsonPath = path.join(baseFolder, 'package.json');
-    const packageJson = await fse.readJson(packageJsonPath);
-    packageJson.exports = exports;
-    await fse.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+    const packageJsonPath = path.join(baseFolder, 'package.json')
+    const packageJson = await fse.readJson(packageJsonPath)
+    packageJson.exports = exports
+    await fse.writeJson(packageJsonPath, packageJson, { spaces: 2 })
   }
   catch (err) {
-    throw new Error('Error updating package.json: ' + err);
+    throw new Error('Error updating package.json: ' + err)
   }
 }
 
 (async () => {
   try {
-    const folders = await readFolders(baseFolder, skips);
-    const exports = generateExports(folders);
-    await updatePackageJson(exports);
+    const folders = await readFolders(baseFolder, skips)
+    const exports = generateExports(folders)
+    await updatePackageJson(exports)
   }
   catch (err) {
-    console.error(err);
+    console.error(err)
   }
-})();
+})()
